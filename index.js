bad962def8ea709fc3cd74a0f8a70f2507c26c83
Mohammed Thasleem MK <mohammedthasleemmk6604@gmail.com>
2025-02-18 15:09:18 +0530

    Third commit

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ app.get('/api/my-posts', authenticateJWT, (req, res) => {
 });
 
 // Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
